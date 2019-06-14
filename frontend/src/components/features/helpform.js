eb557d2d8926c644098f179c3e38852c5f25bc9f
bwpeck <benjaminwade483@yahoo.com>
2019-06-14 13:08:58 -0400

    cleaning things up

---
+++ frontend/src/components/features/helpform.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { addForm } from '../../action';
+
+class HelpForm extends Component {
+
+    state = {
+        StudentName: '',
+        title: '',
+        description: '',
+        categories: '',
+        isLoading: false,
+        id: Date.now(),
+        error: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const { StudentName: name, title: title, description: description, categories: category } = this.state;
+        const newForm = { name, title, description, category };
+        console.log(newForm);
+        this.props.addForm(newForm)
+        this.setState({
+            StudentName: '',
+            title: '',
+            description: '',
+            category: '',
+            isLoading: false,
+            id: Date.now(),
+            error: ''
+        })
+    }
+
+    render() {
+        const { StudentName, title, description, categories} = this.state
+        return (
+            <form
+                className='addHelpForm'
+                onSubmit={this.handleSubmit}>
+                <input
+                    input='text'
+                    name='StudentName'
+                    placeholder='Student Name'
+                    value={StudentName}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input
+                    input='text'
+                    name='Problem'
+                    placeholder='What can we help you with?'
+                    value={title}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input
+                    input='text'
+                    name='Description'
+                    placeholder='Please provide further details'
+                    value={description}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input
+                    input='text'
+                    name='Category'
+                    placeholder='What category is this problem from? i.e React, Redux, HTML, CSS?'
+                    value={categories}
+                    onChange={this.handleChange}
+                    required
+                />
+                <button>Add HelpForm</button>
+            </form>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        StudentName: state.StudentName,
+        title: state.title,
+        description: state.description,
+        categories: state.categories,
+        isLoading: state.isLoading,
+        error: state.error,
+        id: state.id
+    }
+}
+
+
+export default connect(mapStateToProps, { addForm })(HelpForm)
---
+++ frontend/src/components/reducers/index.js
@@ -0,0 +1,51 @@
+import{LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL, ADD_FORM_START, ADD_FORM_SUCCESS, ADD_FORM_FAIL} from '../../action'
+
+const initialState ={
+    login:[],
+    grabData:false,
+    token:localStorage.getItem('token'),
+    error:'',
+  }
+function reducer(state=initialState, action){
+    switch(action.type) {
+        case LOGIN_START:
+        return {
+            ...state,
+            grabData: true,
+            error: ''
+        }
+        case LOGIN_SUCCESS:
+        return{
+            ...state,
+            grabData: true,
+            error:''
+        }
+        case LOGIN_FAIL:
+        return {
+            ...state,
+            grabData: true,
+            error: action.payload,
+        }
+        case ADD_FORM_START:
+        return {
+            ...state,
+            grabData: true,
+            error: '',
+        }
+        case ADD_FORM_SUCCESS:
+        return {
+            ...state,
+            grabData: true,
+            error: '',
+        }
+        case ADD_FORM_FAIL:
+        return {
+            ...state,
+            grabData: true,
+            error: '',
+        }
+        default:
+        return state
+    };
+};
+ export default reducers
